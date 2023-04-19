47c8bc1efbf77f987959ae3e2554226816fbef82
himaja-e <105623310+himaja-e@users.noreply.github.com>
2023-04-19 22:19:59 +0530

    Update 21f1003928.py

--- 21f1003928.py
+++ 21f1003928.py
@@ -1,17 +1,21 @@
-
 import streamlit as st
-a=st.number_input(int(input("Enter the first number")))
-b=st.number_input(int(input("Enter the second number")))
-c=st.number_input(int(input("Enter the third number")))
 
-l=a
-if(a>b and a>c):
-  l=a
-elif(b>a and b>c):
-  l=b
-elif(c>a and c>b):
-  l=c
-else:
-  l=a
+def main():
+  a=st.number_input(int(input("Enter the first number")))
+  b=st.number_input(int(input("Enter the second number")))
+  c=st.number_input(int(input("Enter the third number")))
 
-st.write("Largest number is", l)
+  if(st.button("Find largest")):
+    l=a
+    if(a>b and a>c):
+      l=a
+    elif(b>a and b>c):
+      l=b
+    elif(c>a and c>b):
+      l=c
+    else:
+      l=a
+
+    st.write("Largest number is", l)
+    
+streamlit run Largest 21f1003928.py
